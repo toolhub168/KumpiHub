04b3454f54941b28be0ffc448300d0fd4d7a4d5b
toolhub168 <toolhub76@gmail.com>
2026-08-24 08:33:53 -0700

    Prepare backend for deployment

--- server/server.js
+++ server/server.js
@@ -183,7 +183,7 @@ res.send(Buffer.from(buffer))
   }
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`)
